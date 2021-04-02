cf932eaefd251a56684fe6b2c55cf293f6ff15cc
mccleskeyc <mccleskeyc@gmail.com>
2021-04-02 15:03:51 -0400

    created NavBar.js

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,8 @@ export class About extends Component {
     render() {
         return (
             <div>
-                
+                <h1>About What to Watch</h1>
+                <p>about description here</p>
             </div>
         )
     }
