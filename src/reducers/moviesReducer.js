1ed6c1937f7e37c4312bb564f4669acb75876fb8
mccleskeyc <mccleskeyc@gmail.com>
2021-04-02 14:49:27 -0400

    created moviesReducer

---
+++ src/reducers/moviesReducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+    movies: [],
+    loading: true
+}
+const moviesReducer = (state, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
