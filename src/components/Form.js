b8b2d24ea53578724153f225e20a2ddf4e9c7c48
mccleskeyc <mccleskeyc@gmail.com>
2021-04-02 14:59:25 -0400

    created Form.js

---
+++ src/components/Form.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Form extends Component {
+    render() {
+        return (
+            <div>
+                Recommend a Movie
+            </div>
+        )
+    }
+}
+
+export default Form
