4ca9e1a5d28f243887db54d404caad804fee294d
mccleskeyc <mccleskeyc@gmail.com>
2021-04-02 14:58:38 -0400

    created movieIndex.js

---
+++ src/components/movieIndex.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class movieIndex extends Component {
+    render() {
+        return (
+            <div>
+                List of Movies
+            </div>
+        )
+    }
+}
+
+export default movieIndex
